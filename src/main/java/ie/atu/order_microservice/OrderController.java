2535ea7a1fb0c5f656019d4962939e5da5e01fe1
Patryk Milkiewicz <g00397265@atu.ie>
2024-01-09 13:09:54 +0000

    Added a PUT endpoint that lets the user purchase a product and updates it in the database

--- src/main/java/ie/atu/order_microservice/OrderController.java
+++ src/main/java/ie/atu/order_microservice/OrderController.java
@@ -1,4 +1,18 @@
 package ie.atu.order_microservice;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    private final ProductService productService;
+
+    public OrderController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @PutMapping("/buy/{productId}/{amountWanted}")
+    public String buyProduct(@PathVariable Long productId, @PathVariable int amountWanted){
+        return productService.buyProduct(productId, amountWanted);
+    }
 }
