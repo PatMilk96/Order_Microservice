af9cf8ce4d1614c327ba0f269207ebda8ceea312
Patryk Milkiewicz <g00397265@atu.ie>
2024-01-09 11:21:11 +0000

    added OrderDetails variables with some validation

--- src/main/java/ie/atu/order_microservice/OrderDetails.java
+++ src/main/java/ie/atu/order_microservice/OrderDetails.java
@@ -1,4 +1,34 @@
 package ie.atu.order_microservice;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Data
+@NoArgsConstructor
 public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name cannot be blank")
+    private String name;
+
+    @NotNull(message = "price cannot be empty")
+    @Min(1)
+    private Double price;
+
+    @NotNull(message = "Product Code cannot be empty")
+    private Long productCode;
+
+    @NotNull(message = "Product amount cannot be empty")
+    @Min(0)
+    private Integer amount;
 }
